805c3b2a5908d335b434912ea01b9c8d33422a3f
giovaneneves7 <autornekoyasha@gmail.com>
2024-02-29 14:03:15 -0300

    first commit

---
+++ src/main/java/br/com/ifba/ipss/IndustrialProcessSimulatorSoftware.java
@@ -0,0 +1,12 @@
+package br.com.ifba.ipss;
+
+/**
+ *
+ * @author Giovane Neves
+ */
+public class IndustrialProcessSimulatorSoftware {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

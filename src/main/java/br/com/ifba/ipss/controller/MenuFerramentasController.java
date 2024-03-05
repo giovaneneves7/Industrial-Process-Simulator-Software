c3cc20018c39500e07d4dcabf01d850e7f687199
giovaneneves7 <autornekoyasha@gmail.com>
2024-03-05 15:14:23 -0300

    Feat: Cria controller para o menu de ferramentas

---
+++ src/main/java/br/com/ifba/ipss/controller/MenuFerramentasController.java
@@ -0,0 +1,20 @@
+package br.com.ifba.ipss.controller;
+
+/**
+ *
+ * @author Giovane Neves
+ */
+public class MenuFerramentasController {
+
+    
+    // ****************** { Métodos } ******************//
+    
+    /**
+     * Abre o menu de ferramentas quando um botão é clicado.
+     */
+    private void abrirMenuFerramentas(){
+        
+        /* TODO: Adicionar lógica para abrir menu de ferramentas */
+        
+    }
+}

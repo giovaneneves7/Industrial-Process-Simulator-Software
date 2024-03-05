3beba178b780e6b792fac15f7392e85312087311
giovaneneves7 <autornekoyasha@gmail.com>
2024-03-05 15:35:59 -0300

    Feat: Adiciona métodos base ao controller e cria modelo para ferramentas

--- src/main/java/br/com/ifba/ipss/controller/MenuFerramentasController.java
+++ src/main/java/br/com/ifba/ipss/controller/MenuFerramentasController.java
@@ -1,20 +1,45 @@
 package br.com.ifba.ipss.controller;
 
+import br.com.ifba.ipss.model.Ferramenta;
+
 /**
  *
  * @author Giovane Neves
  */
 public class MenuFerramentasController {
 
-    
+    // *************************************************//
     // ****************** { Métodos } ******************//
+    // *************************************************//
     
     /**
      * Abre o menu de ferramentas quando um botão é clicado.
      */
-    private void abrirMenuFerramentas(){
+    public void abrirMenuFerramentas(){
         
-        /* TODO: Adicionar lógica para abrir menu de ferramentas */
+        /* TODO: Adicionar lógica */
+        
+    }
+    
+    public void fecharMenuFerramentas(){
+        
+        /* TODO: Adicionar lógica */
+        
+    }
+    
+    public void selecionarFerramenta(){
+        /* TODO: Adicionar lógica */
+    }
+    
+    public void adicionarFerramentaAAAreaDeTrabalho(){
+        /* TODO: Adicionar lógica */
+    }
+    
+    public void removerFerramentaDaAreaDeTrabalho(){
+        /* TODO: Adicionar lógica */
+    }
+    
+    public void conectarFerramentas(Ferramenta origem, Ferramenta destino){
         
     }
 }

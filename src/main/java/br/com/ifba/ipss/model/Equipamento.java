21c340229c24056a88a538e567ca5539b2becaf7
giovaneneves7 <autornekoyasha@gmail.com>
2024-03-13 13:28:33 -0300

    feat: Cria modelos base

---
+++ src/main/java/br/com/ifba/ipss/model/Equipamento.java
@@ -0,0 +1,39 @@
+// *************************************************//
+// *************** { COMEÇO - Package } ************//
+// *************************************************//
+package br.com.ifba.ipss.model;
+// *************************************************//
+// *************** { FIM - Package } ***************//
+// *************************************************//
+
+
+// *************************************************//
+// ************ { COMEÇO - Imports } ***************//
+// *************************************************//
+
+
+// *************************************************//
+// ************** { FIM - Imports } ****************//
+// *************************************************//
+
+/**
+ *
+ * @author Giovane Neves
+ * @since V0.0.1
+ */
+public class Equipamento {
+   
+    /* TODO: Adicionar dependência do lomnok quando a internet voltar. */
+
+    // *************************************************//
+    // ***************** { Atributos } *****************//
+    // *************************************************//
+    
+    private String _nome;          // O nome do equipamento.
+    private String _caminhoImagem; // O caminho para a imagem do equipamento.
+    private double _x;             // A posição 'X' do equipamento.
+    private double _y;             // A posição 'Y' do equipamento.
+    private double _largura;       // A largura do equipamento.
+    private double _altura;        // A altura do equipamento.
+    
+} // class Equipamento

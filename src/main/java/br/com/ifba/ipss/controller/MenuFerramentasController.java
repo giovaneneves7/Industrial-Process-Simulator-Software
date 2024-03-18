b797351809458e96faf08aeab9b1adf1a0846915
giovaneneves7 <autornekoyasha@gmail.com>
2024-03-18 15:38:39 -0300

    feat: Adiciona título ao menu

--- src/main/java/br/com/ifba/ipss/controller/MenuFerramentasController.java
+++ src/main/java/br/com/ifba/ipss/controller/MenuFerramentasController.java
@@ -11,6 +11,8 @@ package br.com.ifba.ipss.controller;
 // *************************************************//
 import br.com.ifba.ipss.model.Equipamento;
 import java.awt.Color;
+import java.awt.Font;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import lombok.Data;
 // *************************************************//
@@ -45,10 +47,30 @@ public class MenuFerramentasController {
         int x = (larguraF - 280);
         int y = (alturaF - 820) / 2;
         
-        p.setLayout(null);
+        
+        // Instância do painel
         this._ferramentasContainer = new JPanel();        
+        this._ferramentasContainer.setLayout(null);
+        
+        // Customização do painel
         _ferramentasContainer.setBounds(x, y, 280, 820);
         _ferramentasContainer.setBackground(Color.decode("#5E5E5E"));
+        
+        // Título do painel 
+        JLabel tituloMenu = new JLabel();
+        tituloMenu.setText(nome);
+        Font fonteNegrito = new Font(tituloMenu.getFont().getName(), Font.BOLD, tituloMenu.getFont().getSize());
+        tituloMenu.setFont(fonteNegrito);
+        tituloMenu.setForeground(Color.white);
+        
+        int larguraC = _ferramentasContainer.getWidth();
+        int alturaC = _ferramentasContainer.getHeight(); 
+        int tituloX = larguraC / 2;
+        int tituloY = alturaC - 780;
+       
+        tituloMenu.setBounds(tituloX, tituloY, 50, 20);
+        _ferramentasContainer.add(tituloMenu);
+
         p.add(_ferramentasContainer);
         
         p.setComponentZOrder(_ferramentasContainer, 0);

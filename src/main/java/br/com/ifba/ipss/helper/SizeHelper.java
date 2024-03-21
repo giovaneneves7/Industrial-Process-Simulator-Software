72e6ab6f0961c9ce706c0972e822c43e76067139
giovaneneves7 <autornekoyasha@gmail.com>
2024-03-21 15:17:33 -0300

    feat: Cria pacote para helpers

---
+++ src/main/java/br/com/ifba/ipss/helper/SizeHelper.java
@@ -0,0 +1,24 @@
+// *************************************************//
+// *************** { COMEÇO - Package } ************//
+// *************************************************//
+package br.com.ifba.ipss.helper;
+// *************************************************//
+// *************** { FIM - Package } ***************//
+// *************************************************//
+
+/**
+ * Helper para guardar tamanhos padrões dos elementos que estarão na interface gráfica.
+ * 
+ * @author Giovane Neves
+ * @since V0.0.1
+ */
+public class SizeHelper {
+    
+    // *************************************************//
+    // ***************** { Atributos } *****************//
+    // *************************************************//
+    
+    public static final int ALTURA_FERRAMENTA_CONTAINER = 100;
+    public static final int LARGURA_FERRAMENTA_CONTAINER = 100;
+    
+}

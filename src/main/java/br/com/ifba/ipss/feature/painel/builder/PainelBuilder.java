5b0afd538a1312f14edd8312ca76fc647afde0d0
giovaneneves7 <giovaneneves87@gmail.com>
2024-03-23 20:09:24 -0300

    Cria diretório features

---
+++ src/main/java/br/com/ifba/ipss/feature/painel/builder/PainelBuilder.java
@@ -0,0 +1,60 @@
+package br.com.ifba.ipss.feature.painel.builder;
+
+import br.com.ifba.ipss.feature.painel.model.Painel;
+import java.awt.LayoutManager;
+
+/**
+ *
+ * @author Giovane Neves
+ * @since V0.0.1
+ */
+public class PainelBuilder {
+    
+    private int altura;
+    private int largura;
+    private int x; 
+    private int y;
+    private LayoutManager layout;
+    
+    public PainelBuilder setAltura(int altura){
+        
+        this.altura = altura;
+        return this;
+        
+    }
+    
+    public PainelBuilder setLargura(int largura){
+        
+        this.largura = largura;
+        return this;
+        
+    }
+    
+    public PainelBuilder setX(int x){
+        
+        this.x = x;
+        return this;
+        
+    }
+    
+    public PainelBuilder setY(int y){
+        
+        this.y = y;
+        return this;
+        
+    }
+    
+    public PainelBuilder setLayout(LayoutManager layout){
+        
+        this.layout = layout;
+        return this;
+    }
+    
+    public Painel build(){
+        
+        Painel p = new Painel(this.x, this.altura, this.largura, this.altura);
+        p.setLayout(layout);
+        return p;
+        
+    }
+}
---
+++ src/main/java/br/com/ifba/ipss/feature/tela/model/Tela.java
@@ -0,0 +1,11 @@
+package br.com.ifba.ipss.feature.tela.model;
+
+/**
+ *
+ * @author Giovane Neves
+ */
+public class Tela extends javax.swing.JPanel{
+    
+    
+    
+}

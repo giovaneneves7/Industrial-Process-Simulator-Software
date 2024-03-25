ddc93d13396a924f4004e3888b27444b863f34e1
giovaneneves7 <autornekoyasha@gmail.com>
2024-03-25 19:31:11 -0300

    feat: Cria ViewController

---
+++ src/main/java/br/com/ifba/ipss/controller/ViewController.java
@@ -0,0 +1,30 @@
+package br.com.ifba.ipss.controller;
+
+import br.com.ifba.ipss.helper.PathHelper;
+import br.com.ifba.ipss.util.Constantes;
+import java.awt.Image;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Giovane Neves
+ * @since V0.0.1
+ */
+public class ViewController {
+    
+    public static void definirTituloAplicacao(JFrame f){
+        
+        f.setTitle(Constantes.NOME_APLICACAO);
+        
+    }
+    
+    public static void definirLogoAplicacao(JFrame f, Image img){
+        
+        f.setIconImage(
+                img
+        );
+        
+    }
+    
+}

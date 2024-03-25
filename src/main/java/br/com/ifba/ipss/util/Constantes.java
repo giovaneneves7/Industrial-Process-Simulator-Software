3fffb38fa7c8cdc66882f2c0a4072bdf27ff0a97
giovaneneves7 <autornekoyasha@gmail.com>
2024-03-25 12:03:48 -0300

    feat: Cria diretório 'util'

---
+++ src/main/java/br/com/ifba/ipss/util/Constantes.java
@@ -0,0 +1,14 @@
+package br.com.ifba.ipss.util;
+
+/**
+ *
+ * @author Giovane Neves
+ * @since V0.0.1
+ */
+public class Constantes {
+    
+    public static final String FONTE = "Times New Roman";
+    
+    public static final int DELAY_CARREGAMENTO = 20;
+    
+}

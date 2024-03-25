04d777ba6ed430f3e1567bc0c64a18390f034974
giovaneneves7 <autornekoyasha@gmail.com>
2024-03-25 08:35:02 -0300

    feat: Cria FormulaHelper

---
+++ src/main/java/br/com/ifba/ipss/helper/FormulaHelper.java
@@ -0,0 +1,12 @@
+package br.com.ifba.ipss.helper;
+
+/**
+ *
+ * @author Giovane Neves
+ * @since V0.0.1
+ */
+public class FormulaHelper {
+    
+    private static final double PI = 3.14f;
+    
+}

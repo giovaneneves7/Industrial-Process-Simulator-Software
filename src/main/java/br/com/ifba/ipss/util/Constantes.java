f88bc13221a2f86e0186df2ccc5570296b746a5a
giovaneneves7 <autornekoyasha@gmail.com>
2024-03-25 18:23:33 -0300

    feat: Adiciona constante 'COR_BACKGROUND'

--- src/main/java/br/com/ifba/ipss/util/Constantes.java
+++ src/main/java/br/com/ifba/ipss/util/Constantes.java
@@ -11,8 +11,9 @@ public class Constantes {
     
     public static final String FONTE = "Times New Roman";
     
-    public static Color COR_PRIMARIA = Color.green;
+    public static Color COR_PRIMARIA = new Color(0, 102, 51);
     public static Color COR_SEGUNDARIA = Color.white;
+    public static Color COR_BACKGROUND = Color.decode("#CCCCCC");
     public static Color COR_TEXTO = Color.black;
     
     public static final int DELAY_CARREGAMENTO = 20;

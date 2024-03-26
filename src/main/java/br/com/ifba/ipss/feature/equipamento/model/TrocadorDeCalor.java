b1a7f5dbd593bbe78d898c5ecf27bd96b4db3525
giovaneneves7 <autornekoyasha@gmail.com>
2024-03-26 13:46:06 -0300

    feat: Adiciona novos modelos

---
+++ src/main/java/br/com/ifba/ipss/feature/equipamento/model/TrocadorDeCalor.java
@@ -0,0 +1,12 @@
+package br.com.ifba.ipss.feature.equipamento.model;
+
+/**
+ *
+ * @author Giovane Neves
+ * @since V0.0.1
+ */
+public class TrocadorDeCalor extends Equipamento{
+    
+    
+    
+}

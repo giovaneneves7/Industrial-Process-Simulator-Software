45cfe0dfc2a3dd45e52d5259f7aad0dc7cd8b367
giovaneneves7 <autornekoyasha@gmail.com>
2024-03-26 14:38:08 -0300

    feat: Cria enum para escoamento

---
+++ src/main/java/br/com/ifba/ipss/util/Escoamento.java
@@ -0,0 +1,24 @@
+package br.com.ifba.ipss.util;
+
+/**
+ *
+ * @author Giovane Neves
+ * @since V0.0.1
+ */
+public enum Escoamento {
+    
+    ESCOAMENTO_LAMINAR("Escoamento Laminar"),
+    ESCOAMENTO_TRANSITORIO("Escoamento Transitório"),
+    ESCOAMENTO_TURBULENTO("Escoamento Turubulento");
+    
+    private final String escoamento;
+
+    private Escoamento(final String escoamento) {
+        this.escoamento = escoamento;
+    }
+    
+    public String getString(){
+        return escoamento;
+    }
+    
+}

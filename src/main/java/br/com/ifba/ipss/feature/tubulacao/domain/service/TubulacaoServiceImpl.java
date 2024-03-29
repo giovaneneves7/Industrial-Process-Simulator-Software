79951107ee6d24aecbea96b41bb6b3656412dd94
giovaneneves7 <autornekoyasha@gmail.com>
2024-03-28 21:59:52 -0300

    feat: Cria métodos pegarDi e pegarComprimento

--- src/main/java/br/com/ifba/ipss/feature/tubulacao/domain/service/TubulacaoServiceImpl.java
+++ src/main/java/br/com/ifba/ipss/feature/tubulacao/domain/service/TubulacaoServiceImpl.java
@@ -3,6 +3,7 @@ package br.com.ifba.ipss.feature.tubulacao.domain.service;
 import br.com.ifba.ipss.feature.tubulacao.domain.model.Tubulacao;
 import br.com.ifba.ipss.feature.tubulacao.domain.repository.ITubulacaoRepository;
 import br.com.ifba.ipss.feature.tubulacao.domain.repository.TubulacaoRepositoryImpl;
+import br.com.ifba.ipss.helper.FormulaHelper;
 import java.util.List;
 
 /**
@@ -20,5 +21,21 @@ public class TubulacaoServiceImpl implements ITubulacaoService{
         return tubulacaoRepository.pegarTubulacoes();
 
     }
+
+    @Override
+    public String pegarDiametroInterno(String nomeTubulacao) {
+        
+        return "Di=".concat(tubulacaoRepository.pegarDiametroInterno(nomeTubulacao).concat(FormulaHelper.PES));
+        
+    }
+
+    @Override
+    public String pegarComprimento(String nomeTubulacao) {
+        
+        return "L=".concat(String.valueOf(tubulacaoRepository.pegarComprimento(nomeTubulacao)).concat(FormulaHelper.METRO));
+        
+    }
+    
+    
     
 }

b6ec92f093a3a21cc126d260205fa45af625c19d
giovaneneves7 <autornekoyasha@gmail.com>
2024-03-29 10:45:06 -0300

    refactor: Refatora service de tubulacao

--- src/main/java/br/com/ifba/ipss/feature/tubulacao/domain/model/Tubulacao.java
+++ src/main/java/br/com/ifba/ipss/feature/tubulacao/domain/model/Tubulacao.java
@@ -9,6 +9,7 @@ package br.com.ifba.ipss.feature.tubulacao.domain.model;
 // *************************************************//
 // ************ { COMEÇO - Imports } ***************//
 // *************************************************//
+import br.com.ifba.ipss.common.infrastructure.interfaces.Ferramenta;
 import br.com.ifba.ipss.feature.equipamento.domain.model.Equipamento;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -25,7 +26,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Tubulacao extends Equipamento{
+public class Tubulacao extends Equipamento implements Ferramenta{
     
     // *************************************************//
     // ***************** { Atributos } *****************//

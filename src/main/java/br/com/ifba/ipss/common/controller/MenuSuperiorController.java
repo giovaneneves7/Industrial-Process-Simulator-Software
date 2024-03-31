2ddd35446ebb71335c3837c9bfbe49c88461b869
giovaneneves7 <autornekoyasha@gmail.com>
2024-03-31 19:18:42 -0300

    update: Edita método de girar equipamento

--- src/main/java/br/com/ifba/ipss/common/controller/MenuSuperiorController.java
+++ src/main/java/br/com/ifba/ipss/common/controller/MenuSuperiorController.java
@@ -1,7 +1,13 @@
 package br.com.ifba.ipss.common.controller;
 
 import java.awt.Graphics2D;
+import java.awt.GraphicsConfiguration;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
 import java.awt.Image;
+import java.awt.Transparency;
+import java.awt.geom.AffineTransform;
+import java.awt.image.AffineTransformOp;
 import java.awt.image.BufferedImage;
 import javax.swing.ImageIcon;
 
@@ -11,6 +17,34 @@ import javax.swing.ImageIcon;
  */
 public class MenuSuperiorController {
  
+    public static ImageIcon rotate(BufferedImage image, double angle) {
+        final double rads = Math.toRadians(angle);
+        final double sin = Math.abs(Math.sin(rads));
+        final double cos = Math.abs(Math.cos(rads));
+        final int w = (int) Math.floor(image.getWidth() * cos + image.getHeight() * sin);
+        final int h = (int) Math.floor(image.getHeight() * cos + image.getWidth() * sin);
+        final BufferedImage rotatedImage = new BufferedImage(w, h, image.getType());
+        final AffineTransform at = new AffineTransform();
+        at.translate(w / 2, h / 2);
+        at.rotate(rads, 0, 0);
+        at.translate(-image.getWidth() / 2, -image.getHeight() / 2);
+        final AffineTransformOp rotateOp = new AffineTransformOp(at, AffineTransformOp.TYPE_BILINEAR);
+        rotateOp.filter(image, rotatedImage);
+
+        return new ImageIcon(rotatedImage);
+    }
+
+    public static BufferedImage toBufferedImage(Image image) {
+        if (image instanceof BufferedImage) {
+            return (BufferedImage) image;
+        }
+        BufferedImage bufferedImage = new BufferedImage(image.getWidth(null), image.getHeight(null), BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2d = bufferedImage.createGraphics();
+        g2d.drawImage(image, 0, 0, null);
+        g2d.dispose();
+        return bufferedImage;
+    }
+    
     public static ImageIcon girarEquipamento(ImageIcon imgIcon, int graus) {
         Image img = imgIcon.getImage();
         int largura = img.getWidth(null);

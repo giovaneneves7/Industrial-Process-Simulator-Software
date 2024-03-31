d2859824278887d4932cca83ebd24ef6ed3863e3
giovaneneves7 <autornekoyasha@gmail.com>
2024-03-31 14:03:46 -0300

    feat: Adiciona atributo 'orientacao'

--- src/main/java/br/com/ifba/ipss/feature/label/domain/model/Label.java
+++ src/main/java/br/com/ifba/ipss/feature/label/domain/model/Label.java
@@ -6,11 +6,16 @@ package br.com.ifba.ipss.feature.label.domain.model;
 // *************** { FIM - Package } ***************//
 // *************************************************//
 
+import lombok.Data;
+
 /**
  *
  * @author Giovane Neves
  * @since V0.0.1
  */
+@Data
 public class Label extends javax.swing.JLabel {
+    
+    private String orientacao;
     
 }

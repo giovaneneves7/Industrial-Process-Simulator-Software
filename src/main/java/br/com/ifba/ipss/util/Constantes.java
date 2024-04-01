39f4d96df3f95f2a372f82d404accfe347b104d8
giovaneneves7 <autornekoyasha@gmail.com>
2024-04-01 07:55:41 -0300

    feat: Adiciona valores constantes para nomes de atributos

--- src/main/java/br/com/ifba/ipss/util/Constantes.java
+++ src/main/java/br/com/ifba/ipss/util/Constantes.java
@@ -9,12 +9,15 @@ import java.awt.Color;
  */
 public class Constantes {
     
+    // Informações da Aplicação
     public static final String NOME_APLICACAO = "EduSimLab - Laboratório Virtual de Simulação de Processos";
     public static final String VERSAO = "0.0.1";
     
+    // Tamanhos e Fontes
     public static final String FONTE = "SansSerif";
     public static final int TAMANHO_FONTE = 12;
     
+    // Paleta de Cores
     public static final Color COR_PRIMARIA = new Color(0, 102, 51);
     public static final Color COR_SEGUNDARIA = Color.white;
     public static final Color COR_DARK = Color.decode("#9C9C9C");
@@ -22,8 +25,19 @@ public class Constantes {
     public static final Color COR_BACKGROUND_HOVER = Color.decode("#9C9898"); 
     public static final Color COR_TEXTO = Color.black;
     
+    // Tempos
     public static final int DELAY_CARREGAMENTO = 20;
     
+    // Orientações
     public static final String HORIZONTAL = "horizontal";
     public static final String VERTICAL = "vertical";
+    
+    // Nome de Atributos de Equipamentos
+    public static final String ATRIBUTO_NOME = "_nome";
+    public static final String ATRIBUTO_CAMINHO_IMAGEM = "_caminhoImagem";
+    public static final String ATRIBUTO_POS_X = "_x";
+    public static final String ATRIBUTO_POS_Y = "_y";
+    public static final String ATRIBUTO_LARGURA = "_largura";
+    public static final String ATRIBUTO_ALTURA = "_altura";
+    
 }

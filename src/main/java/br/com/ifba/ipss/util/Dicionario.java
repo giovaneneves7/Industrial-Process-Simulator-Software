55484af748b73184306086d10b287be9c8a1b4a8
giovaneneves7 <autornekoyasha@gmail.com>
2024-04-11 12:58:21 -0300

    feat: Adiciona novas palavras ao dicionário

--- src/main/java/br/com/ifba/ipss/util/Dicionario.java
+++ src/main/java/br/com/ifba/ipss/util/Dicionario.java
@@ -1,6 +1,7 @@
 package br.com.ifba.ipss.util;
 
 import java.util.HashMap;
+import java.util.List;
 
 /**
  *
@@ -9,18 +10,37 @@ import java.util.HashMap;
  */
 public class Dicionario {
     
-    public static final HashMap<String, String> DICIONARIO = new HashMap<>();
+    public static final HashMap<String, List<String>> DICIONARIO = new HashMap<>();
     
     static {
-        
-        DICIONARIO.put("modo_remocao_ativado", "Modo Remoção: Ativado");
-        DICIONARIO.put("modo_remocao_desativado", "Modo Remoção: Desativado");
+
+        DICIONARIO.put("conexao", List.of("Conexão", "Conexões"));
+        DICIONARIO.put("di", List.of("Di"));
+        DICIONARIO.put("equipamento", List.of("Equipamento", "Equipamentos"));
+        DICIONARIO.put("l", List.of("L"));
+        DICIONARIO.put("modo_remocao_ativado", List.of("Modo Remoção: Ativado"));
+        DICIONARIO.put("modo_remocao_desativado", List.of("Modo Remoção: Desativado"));
+        DICIONARIO.put("titulo_aplicacao", List.of(Constantes.NOME_APLICACAO));
+        DICIONARIO.put("tubulacao", List.of("Tubulação", "Tubulações"));
+        DICIONARIO.put("valvula", List.of("Válvula", "Válvulas"));
     }
     
     public static String tr(String chave){
         
         if(DICIONARIO.containsKey(chave))
-            return DICIONARIO.get(chave);
+            return DICIONARIO.get(chave).getFirst();
+        
+        return chave;
+        
+    }
+    
+    public static String trToPlural(String chave){
+        
+        if(DICIONARIO.containsKey(chave)){
+            
+            return (DICIONARIO.get(chave).size() > 1) ? DICIONARIO.get(chave).getLast() : chave;
+            
+        }
         
         return chave;
         

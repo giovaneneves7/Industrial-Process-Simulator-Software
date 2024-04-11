dbb111cf6e4c9ab58b74769a08c521f13fa5b230
giovaneneves7 <autornekoyasha@gmail.com>
2024-04-11 13:00:46 -0300

    update: Edita regras de negócios dos métodos do service

--- src/main/java/br/com/ifba/ipss/feature/tubulacao/domain/service/TubulacaoServiceImpl.java
+++ src/main/java/br/com/ifba/ipss/feature/tubulacao/domain/service/TubulacaoServiceImpl.java
@@ -1,9 +1,12 @@
 package br.com.ifba.ipss.feature.tubulacao.domain.service;
 
+import static br.com.ifba.ipss.util.Dicionario.tr;
+
 import br.com.ifba.ipss.feature.tubulacao.domain.model.Tubulacao;
 import br.com.ifba.ipss.feature.tubulacao.domain.repository.ITubulacaoRepository;
 import br.com.ifba.ipss.feature.tubulacao.domain.repository.TubulacaoRepositoryImpl;
 import br.com.ifba.ipss.helper.FormulaHelper;
+
 import java.util.List;
 
 /**
@@ -25,14 +28,14 @@ public class TubulacaoServiceImpl implements ITubulacaoService{
     @Override
     public String pegarDiametroInterno(String nomeTubulacao) {
         
-        return "Di=".concat(tubulacaoRepository.pegarDiametroInterno(nomeTubulacao).concat(FormulaHelper.PES));
+        return tr("di").concat("=").concat(tubulacaoRepository.pegarDiametroInterno(nomeTubulacao).concat(FormulaHelper.PES));
         
     }
 
     @Override
     public String pegarComprimento(String nomeTubulacao) {
         
-        return "L=".concat(String.valueOf(tubulacaoRepository.pegarComprimento(nomeTubulacao)).concat(FormulaHelper.METRO));
+        return tr("l").concat("=").concat(String.valueOf(tubulacaoRepository.pegarComprimento(nomeTubulacao)).concat(FormulaHelper.METRO));
         
     }
     

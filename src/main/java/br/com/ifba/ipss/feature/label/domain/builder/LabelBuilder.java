3fd042ac44611524584757c7079244bf78323741
giovaneneves7 <autornekoyasha@gmail.com>
2024-04-13 09:03:39 -0300

    feat: Adiciona novo atributo

--- src/main/java/br/com/ifba/ipss/feature/label/domain/builder/LabelBuilder.java
+++ src/main/java/br/com/ifba/ipss/feature/label/domain/builder/LabelBuilder.java
@@ -13,6 +13,8 @@ import br.com.ifba.ipss.feature.label.domain.model.Label;
 import br.com.ifba.ipss.util.Constantes;
 import java.awt.Color;
 import java.awt.Font;
+import java.util.ArrayList;
+import java.util.List;
 // *************************************************//
 // ************** { FIM - Imports } ****************//
 // *************************************************//
@@ -32,74 +34,83 @@ public class LabelBuilder {
     private int largura;
     private int altura;
     private String orientacao = Constantes.VERTICAL;
+    private List<Label> conexoes = new ArrayList<>();
     
     public LabelBuilder setTitulo(String titulo){
         
         this._titulo = titulo;
         
         return this;
-    }
+    } // setTitulo
     
     public LabelBuilder setImagem(javax.swing.ImageIcon imagem){
         
         this._imagem = imagem;
         
         return this;
-    }
+    } // setImagem
     
     public LabelBuilder setBackground(Color background){
         
         this._background = background;
         
         return this;
-    }
+    } // setBackground
     
     public LabelBuilder setForeground(Color foreground){
         
         this._foreground = foreground;
         
         return this;
-    }
+    } // setForeground
     
     public LabelBuilder setFonte(Font fonte){
         
         this.fonte = fonte;
         
         return this;
-    }
+    } // setFonte
     
     public LabelBuilder setLargura(int largura){
         
         this.largura = largura;
         return this;
-    }
+    } // setLargura
     
     public LabelBuilder setAltura(int altura){
         
         this.altura = altura;
         return this;
-    }
+    } // setAltura
     
     public LabelBuilder setOrientacao(String orientacao){
         
         this.orientacao = orientacao;
         return this;
-    }
+    } // setOrientacao
+    
+    public LabelBuilder setConexoes(List<Label> conexoes){
+        
+        this.conexoes = conexoes;
+        return this;
+        
+    } // setConexoes
             
     public Label build(){
         
         Label label = new Label();
         
-        label.setText(_titulo);
-        label.setForeground(_foreground);
-        label.setBackground(_background);
-        label.setIcon(_imagem);
-        label.setBounds(0, 0, largura, altura);
-        label.setFont(fonte);
-        label.setOrientacao(orientacao);
+        label.setText(this._titulo);
+        label.setForeground(this._foreground);
+        label.setBackground(this._background);
+        label.setIcon(this._imagem);
+        label.setBounds(0, 0, this.largura, this.altura);
+        label.setFont(this.fonte);
+        label.setOrientacao(this.orientacao);
+        label.setConexoes(this.conexoes);
         
         return label;
-    }
-    
+        
+    } // build
     
 }

23b0db3dec8ef6fc6d20bdf7859bcdfbfcb4b2e5
giovaneneves7 <autornekoyasha@gmail.com>
2024-05-01 09:37:03 -0300

    feat (manager): Cria diretório manager

--- src/main/java/br/com/ifba/ipss/infrastructure/manager/RepositoryManager.java
+++ src/main/java/br/com/ifba/ipss/infrastructure/manager/RepositoryManager.java
@@ -1,11 +1,20 @@
-package br.com.ifba.ipss.infrastructure.generic;
+package br.com.ifba.ipss.infrastructure.manager;
 
+import br.com.ifba.ipss.feature.conexao.domain.repository.ConexaoRepositoryImpl;
+import br.com.ifba.ipss.feature.conexao.domain.repository.IConexaoRepository;
+import br.com.ifba.ipss.feature.equipamento.domain.repository.EquipamentoRepositoryImpl;
+import br.com.ifba.ipss.feature.equipamento.domain.repository.IEquipamentoRepository;
 import br.com.ifba.ipss.feature.espacotrabalho.domain.repository.EspacoTrabalhoRepositoryImpl;
 import br.com.ifba.ipss.feature.espacotrabalho.domain.repository.IEspacoTrabalhoRepository;
-import br.com.ifba.ipss.infrastructure.interfaces.Implementacao;
-import br.com.ifba.ipss.infrastructure.interfaces.Interface;
+import br.com.ifba.ipss.feature.tubulacao.domain.repository.ITubulacaoRepository;
+import br.com.ifba.ipss.feature.tubulacao.domain.repository.TubulacaoRepositoryImpl;
+import br.com.ifba.ipss.feature.valvula.domain.repository.IValvulaRepository;
+import br.com.ifba.ipss.feature.valvula.domain.repository.ValvulaRepositoryImpl;
+import br.com.ifba.ipss.infrastructure.interfaces.ServiceImpl;
+
 import java.util.HashMap;
 import java.util.Map;
+
 
 /**
  *
@@ -14,19 +23,23 @@ import java.util.Map;
  */
 public class RepositoryManager {
     
-    public static final Map<Class<?>, Implementacao> repositoryMap;
+    private static final Map<Class<?>, ServiceImpl> repositoryMap;
     
     static {
         
         repositoryMap = new HashMap<>(){{
         
             put(IEspacoTrabalhoRepository.class, new EspacoTrabalhoRepositoryImpl());
+            put(ITubulacaoRepository.class, new TubulacaoRepositoryImpl());
+            put(IConexaoRepository.class, new ConexaoRepositoryImpl());
+            put(IEquipamentoRepository.class, new EquipamentoRepositoryImpl());
+            put(IValvulaRepository.class, new ValvulaRepositoryImpl());
             
         }};
         
     }
     
-    public static Implementacao pegarRepositorio(Class<?> i){
+    public static ServiceImpl find(Class<?> i){
         
         if(repositoryMap.containsKey(i)){
             

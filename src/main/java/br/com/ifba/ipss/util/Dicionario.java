d474fb9f834286ffc071b3550ec1c9b9659355ca
giovaneneves7 <autornekoyasha@gmail.com>
2024-06-14 21:31:07 -0300

    update: Adiciona novas entradas

--- src/main/java/br/com/ifba/ipss/util/Dicionario.java
+++ src/main/java/br/com/ifba/ipss/util/Dicionario.java
@@ -28,6 +28,8 @@ public class Dicionario {
 
         DICIONARIO.put("area_de_trabalho_deletada_com_sucesso", List.of("Área de Trabalho deletada com sucesso!"));
         DICIONARIO.put("area_de_trabalho_salva_com_sucesso", List.of("Área de Trabalho salva com sucesso!"));
+        DICIONARIO.put("buscar", List.of("Buscar"));
+        DICIONARIO.put("carregar_simulacao", List.of("Carregar Simulação"));
         DICIONARIO.put("conexao", List.of("Conexão", "Conexões"));
         DICIONARIO.put("deseja_limpar_a_area_de_trabalho", List.of("Deseja limpar a Área de Trabalho?"));
         DICIONARIO.put("deseja_salvar_a_area_de_trabalho", List.of("Deseja salvar a Área de Trabalho?"));
@@ -36,6 +38,7 @@ public class Dicionario {
         DICIONARIO.put("erro", List.of("Erro", "Erros"));
         DICIONARIO.put("erro_ao_deletar_area_trabalho", List.of("Ocorreu um erro ao deletar a área de trabalho!"));
         DICIONARIO.put("erro_ao_salvar_area_trabalho", List.of("Ocorreu um erro ao salvar a área de trabalho!"));
+        DICIONARIO.put("instrucoes", List.of("Instruções"));
         DICIONARIO.put("l", List.of("L"));
         DICIONARIO.put("limpar_area_trabalho", List.of("Limpar Área de Trabalho"));
         DICIONARIO.put("modo_conexao_ativado", List.of("Modo Conexão: Ativado"));
@@ -44,6 +47,7 @@ public class Dicionario {
         DICIONARIO.put("modo_remocao_desativado", List.of("Modo Remoção: Desativado"));
         DICIONARIO.put("modo_rotacao_ativado", List.of("Modo Rotação: Ativado"));
         DICIONARIO.put("modo_rotacao_desativado", List.of("Modo Rotação: Desativado"));
+        DICIONARIO.put("nova_simulacao", List.of("Nova Simulação"));
         DICIONARIO.put("salvar_area_trabalho", List.of("Salvar Área de Trabalho"));
         DICIONARIO.put("service_nao_encontrado", List.of("O serviço requerido não foi encontrado"));
         DICIONARIO.put("titulo_aplicacao", List.of(Constantes.NOME_APLICACAO));

a34119e7b02366cdbd66051f2d1859c5a48e50b3
giovaneneves7 <autornekoyasha@gmail.com>
2024-07-15 15:41:50 -0300

    update: Adicionando comentários padrão

--- src/main/java/br/com/ifba/ipss/feature/equipamento/domain/repository/IEquipamentoRepository.java
+++ src/main/java/br/com/ifba/ipss/feature/equipamento/domain/repository/IEquipamentoRepository.java
@@ -1,13 +1,28 @@
+// *************************************************//
+// *************** { COMEÇO - Package } ************//
+// *************************************************//
 package br.com.ifba.ipss.feature.equipamento.domain.repository;
+// *************************************************//
+// *************** { FIM - Package } ***************//
+// *************************************************//
+
+// *************************************************//
+// ************ { COMEÇO - Imports } ***************//
+// *************************************************//
 
 import br.com.ifba.ipss.feature.equipamento.domain.model.Equipamento;
 import br.com.ifba.ipss.infrastructure.generic.IGenericRepository;
-import java.util.List;
+
+// *************************************************//
+// ************** { FIM - Imports } ****************//
+// *************************************************//
 
 /**
  *
  * @author Giovane Neves
  * @since V0.0.1
+ * 
+ * Repositório da entidade Equipamento.
  */
 public interface IEquipamentoRepository extends IGenericRepository<Equipamento>{
     
